6b19e97d4ee20bf1ea4c5598279fbe14103330df
fcandiotti <fernando_candiotti@hotmail.com>
2022-01-21 07:41:49 -0300

    post orders

--- src/main/java/com/apipedidos/delivery/services/OrderService.java
+++ src/main/java/com/apipedidos/delivery/services/OrderService.java
@@ -1,12 +1,17 @@
 package com.apipedidos.delivery.services;
 
 import com.apipedidos.delivery.dto.OrderDTO;
+import com.apipedidos.delivery.dto.ProductDTO;
 import com.apipedidos.delivery.entities.Order;
+import com.apipedidos.delivery.entities.OrderStatus;
+import com.apipedidos.delivery.entities.Product;
 import com.apipedidos.delivery.repositories.OrderRepository;
+import com.apipedidos.delivery.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,10 +21,25 @@ public class OrderService {
     @Autowired
     OrderRepository repository;
 
+    @Autowired
+    ProductRepository productRepository;
+
     @Transactional(readOnly = true)
     public List<OrderDTO> findAll() {
         List<Order> list = repository.findOrdersWithProducts();
         return list.stream().map(OrderDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO dto) {
+        Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLatitude(), Instant.now(),
+                OrderStatus.PENDING);
+        for (ProductDTO p : dto.getProducts()) {
+            Product product = productRepository.getOne(p.getId());
+            order.getProducts().add(product);
+        }
+        order = repository.save(order);
+        return new OrderDTO(order);
     }
 
 }
