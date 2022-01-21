e302bd58be32a6ac77f64a4d4c87480d65dcdbe4
fcandiotti <fernando_candiotti@hotmail.com>
2022-01-21 06:27:00 -0300

    repositories criados

---
+++ src/main/java/com/apipedidos/delivery/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.apipedidos.delivery.repositories;
+
+import com.apipedidos.delivery.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
