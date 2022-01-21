c636248b09bc37aa6d4fd58c8808b847a7018ddd
fcandiotti <fernando_candiotti@hotmail.com>
2022-01-21 07:06:10 -0300

    OrderDTO criado

---
+++ src/main/java/com/apipedidos/delivery/dto/OrderDTO.java
@@ -0,0 +1,111 @@
+package com.apipedidos.delivery.dto;
+
+import com.apipedidos.delivery.entities.Order;
+import com.apipedidos.delivery.entities.OrderStatus;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OrderDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String address;
+    private Double latitude;
+    private Double longitude;
+    private Instant moment;
+    private Double total;
+    private OrderStatus status;
+
+    private List<ProductDTO> products = new ArrayList<>();
+
+    public OrderDTO() {
+    }
+
+    public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, Double total,
+                    OrderStatus status) {
+        this.id = id;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.moment = moment;
+        this.total = total;
+        this.status = status;
+    }
+
+    public OrderDTO(Order entity) {
+        id = entity.getId();
+        address = entity.getAddress();
+        latitude = entity.getLatitude();
+        longitude = entity.getLongitude();
+        moment = entity.getMoment();
+        total = entity.getTotal();
+        status = entity.getStatus();
+        products = entity.getProducts().stream().map(ProductDTO::new).collect(Collectors.toList());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public List<ProductDTO> getProducts() {
+        return products;
+    }
+
+}
