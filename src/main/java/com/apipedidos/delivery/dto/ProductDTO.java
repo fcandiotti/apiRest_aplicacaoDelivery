46da494409b76bdb3a7d5557c1c52a5b23f77fab
fcandiotti <fernando_candiotti@hotmail.com>
2022-01-21 06:34:53 -0300

    productDTO criado

---
+++ src/main/java/com/apipedidos/delivery/dto/ProductDTO.java
@@ -0,0 +1,74 @@
+package com.apipedidos.delivery.dto;
+
+import com.apipedidos.delivery.entities.Product;
+
+import java.io.Serializable;
+
+public class ProductDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+    private String imgUri;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Long id, String name, Double price, String description, String imgUri) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.imgUri = imgUri;
+    }
+
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        price = entity.getPrice();
+        description = entity.getDescription();
+        imgUri = entity.getImgUri();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgUri() {
+        return imgUri;
+    }
+
+    public void setImgUri(String imgUri) {
+        this.imgUri = imgUri;
+    }
+}
